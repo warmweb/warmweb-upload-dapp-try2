c1c519115dc8dd23d6296459ae153fa21b56061b
East <29425738+eastmaels@users.noreply.github.com>
2025-09-19 13:50:23 +0900

    add vercel speed insights

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import type { Metadata } from 'next'
 import { GeistSans } from 'geist/font/sans'
 import { GeistMono } from 'geist/font/mono'
 import { Analytics } from '@vercel/analytics/next'
+import { SpeedInsights } from '@vercel/speed-insights/next';
 import './globals.css'
 import { WagmiProvider } from "wagmi";
 import { filecoin, filecoinCalibration } from "wagmi/chains";
@@ -50,6 +51,7 @@ export default function RootLayout({
               >
                 <SynapseProvider>
                   {children}
+                  <SpeedInsights />
                 </SynapseProvider>
               </RainbowKitProvider>
             </WagmiProvider>
